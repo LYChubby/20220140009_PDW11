6bc79b521ba84fc2336c57a33c19030307c48226
Fauzi Taufik <f4uzi345@gmail.com>
2024-06-13 18:36:08 +0700

    Commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <img src="https://i.pinimg.com/originals/91/8d/cf/918dcf743df1f2a4086246ef6afc9b0c.gif" className="App-logo" alt="logo" />
+        <p>Anya Forger</p>
+        <a className="App-link" href="https://www.netflix.com/id/title/81511410" target="_blank" rel="noopener noreferrer">
+          SPY X FAMILY
         </a>
       </header>
     </div>
